b30ba1c7aca7cf6cb373109aa44430ae42352107
invzhi <invzhi@protonmail.com>
2024-08-09 22:37:51 +0800

    Setup unary middleware

--- app.go
+++ app.go
@@ -3,30 +3,100 @@ package outward
 import (
 	"context"
 	"net/http"
+	"strings"
 	"syscall"
 
+	validate "github.com/bufbuild/protovalidate-go"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/protovalidate"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/oklog/run"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/invzhi/outward/config"
 	"github.com/invzhi/outward/internal/api"
-	proto "github.com/invzhi/outward/proto/outward/v1"
+	pbv1 "github.com/invzhi/outward/proto/outward/v1"
 )
 
+func grpcLogger() logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, level logging.Level, msg string, fields ...any) {
+		var l zerolog.Level
+
+		switch level {
+		case logging.LevelDebug:
+			l = zerolog.DebugLevel
+		case logging.LevelInfo:
+			l = zerolog.InfoLevel
+		case logging.LevelWarn:
+			l = zerolog.WarnLevel
+		case logging.LevelError:
+			l = zerolog.ErrorLevel
+		default:
+			l = zerolog.NoLevel
+		}
+
+		log.WithLevel(l).Ctx(ctx).Fields(fields).Msg(msg)
+	})
+}
+
 func httpServer(appctx *config.AppContext) (*http.Server, error) {
+	// Setup grpc server.
+	validator, err := validate.New()
+	if err != nil {
+		return nil, err
+	}
+
+	server := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			logging.UnaryServerInterceptor(grpcLogger()),
+			protovalidate.UnaryServerInterceptor(validator),
+			recovery.UnaryServerInterceptor(),
+		),
+	)
+
+	pbv1.RegisterUserServiceServer(server, api.NewUserServer(appctx))
+	pbv1.RegisterWorkspaceServiceServer(server, api.NewWorkspaceServer(appctx))
+
+	// Setup grpc gateway.
 	ctx := context.Background()
 	mux := runtime.NewServeMux()
 
-	err := proto.RegisterUserServiceHandlerServer(ctx, mux, api.NewUserServer(appctx))
-	if err != nil {
-		return nil, err
-	}
-	err = proto.RegisterWorkspaceServiceHandlerServer(ctx, mux, api.NewWorkspaceServer(appctx))
+	conn, err := grpc.NewClient(appctx.Conf.Address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		return nil, err
 	}
 
-	return &http.Server{Addr: appctx.Conf.Address, Handler: mux}, nil
+	if err := pbv1.RegisterUserServiceHandler(ctx, mux, conn); err != nil {
+		return nil, err
+	}
+	if err := pbv1.RegisterWorkspaceServiceHandler(ctx, mux, conn); err != nil {
+		return nil, err
+	}
+
+	// Setup http endpoint.
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		contentType := r.Header.Get("Content-Type")
+		if strings.HasPrefix(contentType, "application/grpc") {
+			server.ServeHTTP(w, r)
+			return
+		}
+
+		mux.ServeHTTP(w, r)
+	})
+	handler := h2c.NewHandler(h, &http2.Server{})
+
+	s := http.Server{Addr: appctx.Conf.Address, Handler: handler}
+	s.RegisterOnShutdown(func() { _ = conn.Close() })
+
+	return &s, nil
 }
 
 type App struct {
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/invzhi/outward
 go 1.21.11
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.31.0-20230802163732-1c33ebd9ecfa.1
 	github.com/ardanlabs/conf/v3 v3.1.7
+	github.com/bufbuild/protovalidate-go v0.2.1
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.1.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0
 	github.com/jackc/pgx-zerolog v0.0.0-20230315001418-f978528409eb
 	github.com/jackc/pgx/v5 v5.6.0
@@ -13,21 +16,25 @@ require (
 	github.com/samber/lo v1.46.0
 	github.com/slack-go/slack v0.13.0
 	github.com/sony/sonyflake v1.2.0
+	golang.org/x/crypto v0.24.0
+	golang.org/x/net v0.26.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240730163845-b1a4ccb954bf
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
 )
 
 require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
+	github.com/google/cel-go v0.17.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
